4a9ee68ed199b47949886896b0ed5b2cec0db920
Gregstr <agregorica@gmail.com>
2023-01-20 17:55:36 +0100

    added new card backfaces [ backfaceB.png, backfaceR.png ]

---
+++ load_structs.c
@@ -0,0 +1,70 @@
+//
+// Created by Gregstr on 19/01/2023.
+//
+
+#include "load_structs.h"
+
+
+struct Cards loadStructs(Cards deck)
+{
+
+    deck.hA = LoadTexture("../img/hearts/Ace.png");
+    deck.h2 = LoadTexture("../img/hearts/Two.png");
+    deck.h3 = LoadTexture("../img/hearts/Three.png");
+    deck.h4 = LoadTexture("../img/hearts/Four.png");
+    deck.h5 = LoadTexture("../img/hearts/Five.png");
+    deck.h6 = LoadTexture("../img/hearts/Six.png");
+    deck.h7 = LoadTexture("../img/hearts/Seven.png");
+    deck.h8 = LoadTexture("../img/hearts/Eight.png");
+    deck.h9 = LoadTexture("../img/hearts/Nine.png");
+    deck.h10 = LoadTexture("../img/hearts/Ten.png");
+    deck.hJ = LoadTexture("../img/hearts/Jack.png");
+    deck.hQ = LoadTexture("../img/hearts/Queen.png");
+    deck.hK = LoadTexture("../img/hearts/King.png");
+
+    deck.cA = LoadTexture("../img/clubs/Ace.png");
+    deck.c2 = LoadTexture("../img/clubs/Two.png");
+    deck.c3 = LoadTexture("../img/clubs/Three.png");
+    deck.c4 = LoadTexture("../img/clubs/Four.png");
+    deck.c5 = LoadTexture("../img/clubs/Five.png");
+    deck.c6 = LoadTexture("../img/clubs/Six.png");
+    deck.c7 = LoadTexture("../img/clubs/Seven.png");
+    deck.c8 = LoadTexture("../img/clubs/Eight.png");
+    deck.c9 = LoadTexture("../img/clubs/Nine.png");
+    deck.c10 = LoadTexture("../img/clubs/Ten.png");
+    deck.cJ = LoadTexture("../img/clubs/Jack.png");
+    deck.cQ = LoadTexture("../img/clubs/Queen.png");
+    deck.cK = LoadTexture("../img/clubs/King.png");
+
+    deck.dA = LoadTexture("../img/diamonds/Ace.png");
+    deck.d2 = LoadTexture("../img/diamonds/Two.png");
+    deck.d3 = LoadTexture("../img/diamonds/Three.png");
+    deck.d4 = LoadTexture("../img/diamonds/Four.png");
+    deck.d5 = LoadTexture("../img/diamonds/Five.png");
+    deck.d6 = LoadTexture("../img/diamonds/Six.png");
+    deck.d7 = LoadTexture("../img/diamonds/Seven.png");
+    deck.d8 = LoadTexture("../img/diamonds/Eight.png");
+    deck.d9 = LoadTexture("../img/diamonds/Nine.png");
+    deck.d10 = LoadTexture("../img/diamonds/Ten.png");
+    deck.dJ = LoadTexture("../img/diamonds/Jack.png");
+    deck.dQ = LoadTexture("../img/diamonds/Queen.png");
+    deck.dK = LoadTexture("../img/diamonds/King.png");
+
+    deck.sA = LoadTexture("../img/spades/Ace.png");
+    deck.s2 = LoadTexture("../img/spades/Two.png");
+    deck.s3 = LoadTexture("../img/spades/Three.png");
+    deck.s4 = LoadTexture("../img/spades/Four.png");
+    deck.s5 = LoadTexture("../img/spades/Five.png");
+    deck.s6 = LoadTexture("../img/spades/Six.png");
+    deck.s7 = LoadTexture("../img/spades/Seven.png");
+    deck.s8 = LoadTexture("../img/spades/Eight.png");
+    deck.s9 = LoadTexture("../img/spades/Nine.png");
+    deck.s10 = LoadTexture("../img/spades/Ten.png");
+    deck.sJ = LoadTexture("../img/spades/Jack.png");
+    deck.sQ = LoadTexture("../img/spades/Queen.png");
+    deck.sK = LoadTexture("../img/spades/King.png");
+
+    deck.backfaceB = LoadTexture("../img/backs/kittyback-blue.png");
+    deck.backfaceR = LoadTexture("../img/backs/kittyback-red.png");
+
+}
